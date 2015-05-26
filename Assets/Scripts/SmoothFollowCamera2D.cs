50d681c4b23e7589150adafc9ff8abeaf3dd1cbd
valentemesmo <valentemesmo@gmail.com>
2015-05-26 00:13:08 -0300

    close #9 Make camera move when reach edge of screen

---
+++ Assets/Scripts/SmoothFollowCamera2D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmoothFollowCamera2D : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 velocity = Vector3.zero;
+
+    public float smoothTime = 0.15f;
+
+    public float difference = 4f;
+
+    public float minY = 0f;
+
+    void Update()
+    {
+        if (target)
+        {
+            Vector3 targetPosition = transform.position;
+
+            var heightDifference = target.position.y - transform.position.y;
+
+            if ((heightDifference < -difference || heightDifference > difference))
+            {
+                if (target.position.y > minY)
+                    targetPosition.y = target.position.y;
+                else
+                    targetPosition.y = minY;
+            }
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
