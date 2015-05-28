64373253165d44e21514a9f32e5e79b8be2a375b
valentemesmo <valentemesmo@gmail.com>
2015-05-28 01:04:41 -0300

    Vibrate smartphone on collision
    close #11

---
+++ Assets/Scripts/VibrationComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VibrationComponent : MonoBehaviour
+{
+    public long milliseconds;
+
+    protected override void OnAwake()
+    {
+        new VibrateOnCollision(new VibrationHandler(), this, milliseconds);
+    }
+}
+
+public class VibrateOnCollision
+{
+    IVibration Vibration;
+    long Milliseconds;
+
+    public VibrateOnCollision(IVibration vibration, ICollider2D collider, long milliseconds)
+    {
+        Vibration = vibration;
+        Milliseconds = milliseconds;
+        collider.OnCollisionEnter += collider_OnCollisionEnter;
+    }
+
+    void collider_OnCollisionEnter(object sender, Collision2DEventArgs e)
+    {
+        Vibration.Vibrate(Milliseconds);
+    }
+}
