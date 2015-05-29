8e6ae27c15d4377ae9d3c43748cd7c87002760aa
valentemesmo <valentemesmo@gmail.com>
2015-05-29 00:52:18 -0300

    Fix android audio that was crashing the game
    close #3

--- Assets/Scripts/AudioClipPlus.cs
+++ Assets/Scripts/AudioClipPlus.cs
@@ -3,61 +3,19 @@ using UnityEngine;
 
 public class AudioClipPlus : MonoBehaviour
 {
-    public AudioClip Audio;
     public string StreamingAssetsFilePath;
+    private AudioHandler AudioHandler;
+    public AudioClip audioClip;
 
-    private AudioSource AudioSource;
-
-#if UNITY_ANDROID && !UNITY_EDITOR
-    private AndroidJavaClass unityActivityClass;
-    private AndroidJavaObject activityObj;
-    private AndroidJavaObject soundObj;
-    private int soundPoolAudioId;
-
-    void Start()
+    protected override void OnAwake()
     {
-        if (string.IsNullOrEmpty(StreamingAssetsFilePath) == false)
-        {
-            unityActivityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            activityObj = unityActivityClass.GetStatic<AndroidJavaObject>("currentActivity");
-            soundObj = new AndroidJavaObject("com.catsknead.androidsoundfix.AudioCenter", 1, activityObj, activityObj);
-            soundPoolAudioId = soundObj.Call<int>("loadSound", new object[] { StreamingAssetsFilePath });
-        }
-        else
-        {
-            AudioSource = gameObject.AddComponent<AudioSource>();
-            AudioSource.clip = Audio;
-        }
+        var source = gameObject.AddComponent<AudioSource>();
+        source.clip = audioClip;
+        AudioHandler = new AudioHandler(source, StreamingAssetsFilePath);
     }
 
     public void Play()
     {
-        if (string.IsNullOrEmpty(StreamingAssetsFilePath) == false)
-        {
-            try
-            {
-                soundObj.Call("playSound", new object[] { soundPoolAudioId });
-            }
-            catch (Exception ex)
-            {
-                Debug.Log(ex.ToString());
-            }
-        }
-        else
-            AudioSource.Play();
+        AudioHandler.Play();
     }
-#else
-
-    void Start()
-    {
-        AudioSource = gameObject.AddComponent<AudioSource>();
-        AudioSource.clip = Audio;
-    }
-
-    public void Play()
-    {
-        AudioSource.Play();
-    }
-
-#endif
 }
