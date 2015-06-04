99d99e397dc00d1cb2b5110df5f7875958ce6c61
valentemesmo <valentemesmo@gmail.com>
2015-06-04 18:49:17 -0300

    Removing camera movement

--- Assets/Scripts/SmoothFollowCamera2D.cs
+++ Assets/Scripts/SmoothFollowCamera2D.cs
@@ -4,31 +4,55 @@ using System.Collections;
 public class SmoothFollowCamera2D : MonoBehaviour
 {
     public Transform target;
-    private Vector3 velocity = Vector3.zero;
+    //private Vector3 velocity = Vector3.zero;
 
-    public float smoothTime = 0.15f;
+    public float difference = 14f;
 
-    public float difference = 4f;
+    public float minY = 22.5f;
 
-    public float minY = 0f;
+    //public float dampTime = 0.15f;
+    public float t = 0.15f;
 
-    void Update()
+    private Vector3 targetPosition;
+
+    protected override void OnAwake()
     {
-        if (target)
-        {
-            Vector3 targetPosition = transform.position;
+        targetPosition = transform.position;
+        base.OnAwake();
+    }
 
-            var heightDifference = target.position.y - transform.position.y;
+    void LateUpdate()
+    {
+        Vector3 point = Camera.main.WorldToViewportPoint(target.position);
+        Vector3 delta = target.position - Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
 
-            if ((heightDifference < -difference || heightDifference > difference))
-            {
-                if (target.position.y > minY)
-                    targetPosition.y = target.position.y;
-                else
-                    targetPosition.y = minY;
-            }
+        if ((delta.y < -difference || delta.y > difference))
+            targetPosition = target.position;
 
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
-        }
+
+        var newPosition = transform.position + t * (targetPosition - transform.position).normalized;
+        if (newPosition.y < minY)
+            newPosition.y = minY;
+
+        transform.position = new Vector3(transform.position.x,newPosition.y,transform.position.z);
+
+
+
+        //if (target)
+        //{
+        //    Vector3 targetPosition = transform.position;
+
+        //    var heightDifference = target.position.y - transform.position.y;
+
+        //    if ((heightDifference < -difference || heightDifference > difference))
+        //    {
+        //        if (target.position.y > minY)
+        //            targetPosition.y = target.position.y;
+        //        else
+        //            targetPosition.y = minY;
+        //    }
+
+        //    transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        //}
     }
 }
