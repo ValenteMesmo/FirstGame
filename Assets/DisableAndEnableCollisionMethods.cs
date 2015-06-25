e54117d768be5a626f1a86a7502d0776c9500d72
valentemesmo <valentemesmo@gmail.com>
2015-06-25 00:10:54 -0300

    Enemy wave #42

---
+++ Assets/DisableAndEnableCollisionMethods.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class DisableAndEnableCollisionMethods : MonoBehaviour
+{
+    Collider2D Collider;
+    void Start()
+    {
+        Collider = GetComponent < Collider2D>();
+
+    }
+
+    void EnableCollisions()
+    {
+        Collider.enabled = true;
+    }
+
+    void DisableCollisions()
+    {
+        Collider.enabled = false;
+    }
+}
---
+++ Assets/SetAnimatorBooleanOnCollisionEnter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class SetAnimatorBooleanOnCollisionEnter : MonoBehaviour
+{
+    Animator Animator;
+    public string ParameterName = "";
+    public bool ParameterValue = true;
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Animator.SetBool(ParameterName,true);
+    }
+}
