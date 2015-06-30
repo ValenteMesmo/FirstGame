0f9561a5f4f551b465a15873e8cadee276536822
valentemesmo <valentemesmo@gmail.com>
2015-06-29 22:38:00 -0300

    Improving main table design

---
+++ Assets/MoveToPointWhenLeftKeyIsPressed.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToPointWhenLeftKeyIsPressed : MonoBehaviour
+{
+    public Vector2 TargetPosition;
+    private Vector2 OriginalPosition;
+
+    bool pressed;
+
+    void Start()
+    {
+        OriginalPosition = transform.position;
+        var inputs = GlobalComponents.Get<ControlsPlayerInputs>();
+        inputs.LeftButtonUp += inputs_LeftButtonUp;
+        inputs.LeftButtonDown += inputs_LeftButtonDown;
+    }
+
+    void inputs_LeftButtonDown(object sender, System.EventArgs e)
+    {
+        pressed = true;
+    }
+
+    void inputs_LeftButtonUp(object sender, System.EventArgs e)
+    {
+        pressed = false;
+    }
+
+    void Update()
+    {
+        if (pressed)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, TargetPosition, 0.5f);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, OriginalPosition, 0.5f);
+        }
+    }
+}
---
+++ Assets/MoveToPointWhenRightKeyIsPressed.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToPointWhenRightKeyIsPressed : MonoBehaviour
+{
+    public Vector2 TargetPosition;
+    private Vector2 OriginalPosition;
+
+    bool pressed;
+
+    void Start()
+    {
+        OriginalPosition = transform.position;
+        var inputs = GlobalComponents.Get<ControlsPlayerInputs>();
+        inputs.RightButtonUp += inputs_RightButtonUp;
+        inputs.RightButtonDown += inputs_RightButtonDown;
+    }
+
+    void inputs_RightButtonDown(object sender, System.EventArgs e)
+    {
+        pressed = true;
+    }
+
+    void inputs_RightButtonUp(object sender, System.EventArgs e)
+    {
+        pressed = false;
+    }
+
+    void Update()
+    {
+        if (pressed)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, TargetPosition, 0.5f);
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, OriginalPosition, 0.5f);
+        }
+    }
+}
