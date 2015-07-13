ba7700d5565aaac109908e81f4aa3e4c1c292a2e
valentemesmo <valentemesmo@gmail.com>
2015-07-13 00:08:34 -0300

    Fix firefly position on webgl version
    close #54

--- Assets/WarpWhenXLowerThenValue.cs
+++ Assets/WarpWhenXLowerThenValue.cs
@@ -6,11 +6,6 @@ public class WarpWhenXLowerThenValue : MonoBehaviour
     public float XValue = 0f;
     public float WarpDistance = 50f;
 
-    void Start()
-    {
-        XValue = Camera.main.ScreenToWorldPoint(new Vector2(XValue,0)).x;        
-    }
-
     void Update()
     {
         if (transform.position.x < XValue)
