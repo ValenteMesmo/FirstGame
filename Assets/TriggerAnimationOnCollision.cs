3acb2deb8dc9cdd04baf1cdbe0868cbe1b6dba8d
valentemesmo <valentemesmo@gmail.com>
2015-08-04 16:47:39 -0300

    :godmode:

---
+++ Assets/TriggerAnimationOnCollision.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerAnimationOnCollision : MonoBehaviour
+{
+    Animator Animator;
+    public string triggerName;
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.tag == "Player")
+        {
+            Animator.SetTrigger(triggerName);
+        }
+    }
+}
