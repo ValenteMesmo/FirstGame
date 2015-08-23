e4c2b4b78163c4d91ad3aae04aa7a38e26efc537
valentemesmo <valentemesmo@gmail.com>
2015-08-23 00:44:02 -0300

    Button press animation
    close #66

--- Assets/Scripts/DetectTouchOnThisGameObject.cs
+++ Assets/Scripts/DetectTouchOnThisGameObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 using UnitySolution.InputComponents;
 
 public class DetectTouchOnThisGameObject : MonoBehaviour
---
+++ Assets/SetAnimatorBoolOnTouch.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(DetectTouchOnThisGameObject))]
+[RequireComponent(typeof(Animator))]
+public class SetAnimatorBoolOnTouch : MonoBehaviour
+{
+    public string parameterName = "down";
+
+    Animator Animator;
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        var touch = GetComponent<DetectTouchOnThisGameObject>();
+        touch.OnTouch += touch_OnTouch;
+        touch.OffTouch += touch_OffTouch;
+    }
+
+    void touch_OffTouch(object sender, UnitySolution.InputComponents.TransformEvevntArgs e)
+    {
+        Animator.SetBool(parameterName, false);
+    }
+
+    void touch_OnTouch(object sender, UnitySolution.InputComponents.TransformEvevntArgs e)
+    {
+        Animator.SetBool(parameterName, true);
+    }
+
+}
