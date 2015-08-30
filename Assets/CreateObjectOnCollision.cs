95d9513c027a74c7206f9aa26826053daa959ac4
valentemesmo <valentemesmo@gmail.com>
2015-08-30 15:48:31 -0300

    :goberserk:

---
+++ Assets/CreateObjectOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateObjectOnCollision : MonoBehaviour
+{
+
+    public GameObject ObjectPrefab;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+            Instantiate(ObjectPrefab, transform.position,  Quaternion.identity);
+
+    }
+
+}
---
+++ Assets/IHaveDestroyMethod.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IHaveDestroyMethod : MonoBehaviour {
+
+	public void DestroyItSelf()
+    {
+        Destroy(transform.gameObject);
+    }
+}
