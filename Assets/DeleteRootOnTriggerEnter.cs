6e0fe9c882c393933b0c5fc850f1986c81b2bbee
valentemesmo <valentemesmo@gmail.com>
2015-09-30 00:54:09 -0300

    Black layer covering table on start
    close #78

---
+++ Assets/DeleteRootOnTriggerEnter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteRootOnTriggerEnter : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+            Destroy(transform.root.gameObject);
+    }
+}
