03cf13673ace743334b61002755e0b132a0e33ff
valentemesmo <valentemesmo@gmail.com>
2015-10-25 19:14:27 -0200

    Fixing sorting layer of TrailRenderer component

---
+++ Assets/TrailRendererSortingLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(TrailRenderer))]
+public class TrailRendererSortingLayer : MonoBehaviour
+{
+    public string SortingLayerName = "Background";
+    public int SortingOrder = 0;
+
+    void Start()
+    {
+        var trail = GetComponent<TrailRenderer>();
+
+        trail.sortingLayerName = SortingLayerName;
+        trail.sortingOrder = SortingOrder;
+    }
+}
