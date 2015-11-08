d2f844a0c73aa8f0679ec36272b79172cb6e44c1
valentemesmo <valentemesmo@gmail.com>
2015-11-08 10:15:14 -0200

    close #92

---
+++ Assets/SetAnimatorBoolWhenTargetYLowerThan.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SetAnimatorBoolWhenTargetYLowerThan : MonoBehaviour
+{
+    public Transform Target;
+    private Animator Animator;
+    public float YValue = 0f;
+    public string ParameterName;
+
+    void Start()
+    {
+        if (Target == null)
+            throw new NullReferenceException("Target can not be null!");
+        Animator = GetComponent<Animator>();
+        if (Animator == null)
+            throw new NullReferenceException("Animator can not be null!");
+        if (ParameterName == null)
+            throw new NullReferenceException("ParameterName can not be null!");
+    }
+
+    void Update()
+    {
+        if (Target.transform.position.y < YValue)
+            Animator.SetBool(ParameterName, true);
+    }
+}
