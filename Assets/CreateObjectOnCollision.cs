0245f655821c78817fa2e5c21e2bb662e287e4e3
valentemesmo <valentemesmo@gmail.com>
2015-11-09 22:30:12 -0200

    close #95

--- Assets/CreateObjectOnCollision.cs
+++ Assets/CreateObjectOnCollision.cs
@@ -3,14 +3,20 @@ using System.Collections;
 
 public class CreateObjectOnCollision : MonoBehaviour
 {
-
     public GameObject ObjectPrefab;
+    public bool CreateOnOtherColliderPosition = true;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Player")
-            Instantiate(ObjectPrefab, transform.position,  Quaternion.identity);
+        {
+            Vector2 pos;
+            if (CreateOnOtherColliderPosition)
+                pos = collision.collider.transform.position;
+            else
+                pos = transform.position;
 
+            Instantiate(ObjectPrefab, pos, Quaternion.identity);
+        }
     }
-
 }
