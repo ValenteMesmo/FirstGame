fa6bc5ff86ef44dbb5421fcbca2a26411ff77e2b
valentemesmo <valentemesmo@gmail.com>
2015-11-19 01:22:36 -0200

    :godmode:

---
+++ Assets/SetAnimatorBoolOnTriggerEnter.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SetAnimatorBoolOnTriggerEnter : MonoBehaviour
+{
+    public string ParameterName;
+    private Animator Animator;
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+        if (Animator == null)
+            throw new NullReferenceException("Animator");
+        if (ParameterName == null)
+            throw new NullReferenceException("ParameterName");
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+            Animator.SetBool(ParameterName, true);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+            Animator.SetBool(ParameterName, false);
+    }
+}
