4e159f40837c5d7f272756445a130cf15e3e6cec
valentemesmo <valentemesmo@gmail.com>
2015-11-25 00:42:26 -0200

    close #105

---
+++ Assets/VibrateOnTriggerExit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class VibrateOnTriggerExit : MonoBehaviour
+{
+    IVibration VibrationHandler;
+    public long duration = 100;
+
+    void Start()
+    {
+        VibrationHandler = new VibrationHandler(this);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+            VibrationHandler.Vibrate(duration);
+    }
+}
