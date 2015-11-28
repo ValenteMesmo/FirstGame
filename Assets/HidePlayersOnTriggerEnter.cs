79b7350654658b4086c0211013391fa8b08b2476
valentemesmo <valentemesmo@gmail.com>
2015-11-28 21:44:28 -0200

    character disappear when swallowed by the frog

---
+++ Assets/HidePlayersOnTriggerEnter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HidePlayersOnTriggerEnter : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponentInChildren<TrailRenderer>().enabled = false;
+            col.transform.parent.GetComponentInChildren<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.tag == "Player")
+        {
+            col.GetComponentInChildren<TrailRenderer>().enabled = true;
+            col.transform.parent.GetComponentInChildren<SpriteRenderer>().enabled = true;
+        }
+    }
+
+}
