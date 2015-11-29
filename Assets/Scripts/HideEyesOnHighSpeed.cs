b9183c3296247ac1190ffaa179b72fcdaab4b91a
valentemesmo <valentemesmo@gmail.com>
2015-11-29 21:51:03 -0200

    :godmode:

---
+++ Assets/Scripts/HideEyesOnHighSpeed.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideEyesOnHighSpeed : MonoBehaviour
+{
+    public SpriteRenderer SpriteRenderer;
+    public Rigidbody2D Rigidbody2D;
+
+    public float speedToCHangeAnimation = 8f;
+    Color oldCOlor;
+    Color invisible = new Color(0, 0, 0, 0);
+    void Awake()
+    {
+        oldCOlor = SpriteRenderer.color;
+    }
+
+    void Update()
+    {
+        if (IsMovingOnHighVelocity())
+            SpriteRenderer.color = invisible;
+        else
+            SpriteRenderer.color = oldCOlor;
+    }
+
+    private bool IsMovingOnHighVelocity()
+    {
+        return Rigidbody2D.velocity.x < -speedToCHangeAnimation ||
+            Rigidbody2D.velocity.y < -speedToCHangeAnimation ||
+            Rigidbody2D.velocity.x > speedToCHangeAnimation ||
+            Rigidbody2D.velocity.y > speedToCHangeAnimation;
+    }
+}
---
+++ Assets/SetBoolOnCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetBoolOnCollision : MonoBehaviour
+{
+    public Animator TargetAnimator;
+    public string Parameter;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude >= 3f)
+            TargetAnimator.SetTrigger(Parameter);
+    }
+}
